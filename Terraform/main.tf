b47c7b4a9d4f3b7ea91fa7675af4f665b2afa36f
sirishap03 <ponnadasirisha01@gmail.com>
2025-05-13 15:15:55 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-085386e29e44dacd7" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome to Harness EC2 Instance</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Harness-trail"
+  }
+}
